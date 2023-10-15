1d76edcd4a0673f736c049ed1a4bb29a66873c1a
DavillScript <147995291+DavillScript@users.noreply.github.com>
2023-10-16 01:14:03 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,5 +1,4 @@
 function server(){
-  $r =  str_repeat("\033[1;95m~", 53)."\n";
   pil:
     print "\033[1;92m1. Claimbits\n";
     print "\033[1;92m2. Claimfreecoins\n";
@@ -7,7 +6,6 @@ function server(){
     print "\033[1;92m4. Balkanofaucet\n";
     print "\033[1;92m5. Exit\n";
     $pil["pilih"]=readline("\033[1;97mInput Your Number : \033[1;92m");
-    print"\033[1;95m$r";
     if($pil["pilih"]==1){
       eval(file_get_contents('https://raw.githubusercontent.com/DavillScript/Server/main/bot.php'));
     }elseif($pil["pilih"]==2){
