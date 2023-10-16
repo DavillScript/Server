df9c0da2d2d6640a5961c1db81da850321a9954c
DavillScript <147995291+DavillScript@users.noreply.github.com>
2023-10-16 01:23:59 -0700

    Update server.php

--- server.php
+++ server.php
@@ -1,10 +1,10 @@
 function server(){
   $r =  str_repeat("\033[1;95m~", 53)."\n";
   pil:
-    print "\033[1;92m1. Claimbits => https://tutwuri.id/HdikAH5\n";
+    print "\033[1;92m1. Claimbits      => https://tutwuri.id/HdikAH5\n";
     print "\033[1;92m2. Claimfreecoins => https://tutwuri.id/zuLacYmL\n";
-    print "\033[1;92m3. Faucetpk => https://tutwuri.id/1yNECie\n";
-    print "\033[1;92m4. Balkanofaucet => https://tutwuri.id/k7nDp\n";
+    print "\033[1;92m3. Faucetpk       => https://tutwuri.id/1yNECie\n";
+    print "\033[1;92m4. Balkanofaucet  => https://tutwuri.id/k7nDp\n";
     print "\033[1;92m5. Exit\n";
     $pil["pilih"]=readline("\033[1;97mInput Your Number : \033[1;92m");
     print"\033[1;95m$r";
