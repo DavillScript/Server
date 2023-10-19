84742245636593625dbfdf04993e9933f9ec58f2
DavillScript <147995291+DavillScript@users.noreply.github.com>
2023-10-19 23:03:52 +0700

    Update server.php

--- server.php
+++ server.php
@@ -6,7 +6,7 @@ function server(){
     print "\033[1;92m2. Claimfreecoins => https://tutwuri.id/zuLacYmL\n";
     print "\033[1;92m3. Faucetpk       => https://tutwuri.id/1yNECie\n";
     print "\033[1;92m4. Balkanofaucet  => https://tutwuri.id/k7nDp\n";
-    print "\033[1;92m5. trxfarms       => https://tutwuri.id/2tGfg69W\n";
+    print "\033[1;92m5. Krkbtc Mining  => https://tutwuri.id/ahseA\n";
     print "\033[1;92m6. Exit\n";
     $pil["pilih"]=readline("\033[1;97mInput Your Number : \033[1;92m");
     print"\033[1;95m$r";
@@ -19,8 +19,7 @@ function server(){
     }elseif($pil["pilih"]==4){
       eval(file_get_contents('https://raw.githubusercontent.com/DavillScript/Server/main/Balkanofaucet'));
     }elseif($pil["pilih"]==5){
-       echo "\033[1;91mthis is script need update by: Yadi Supriyadi\n";
-      #eval(file_get_contents('https://raw.githubusercontent.com/DavillScript/Server/main/trxfarms'));
+      eval(file_get_contents('https://raw.githubusercontent.com/DavillScript/Server/main/Mining'));
     }elseif($pil["pilih"]==6){
       exit;
     }else{
