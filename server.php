110ba3597245f34a38869f92cdca1fe9bb1bd4a1
DavillScript <147995291+DavillScript@users.noreply.github.com>
2023-10-25 01:49:12 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,4 +1,5 @@
 function server(){
   system('termux-open https://www.highcpmrevenuegate.com/k99xk5vp03?key=d61cbbf2439c9dbd81a43bf04c8dec95');
   echo "Server maintenance please wait\n";
+  exit;
 }
